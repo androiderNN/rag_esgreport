76e26e33226d4f66763ff84904d7bb9701a58c7a
androiderNN <kento.i.1103@gmail.com>
2025-01-21 19:52:03 +0900

    pypdfとpypdfium両方を実装 前者は文字化けする、後者は行の末端が抜け落ち数値等を誤る傾向がありそう 条件分岐などで使い分けてもよいか

--- src/pdf2text.py
+++ src/pdf2text.py
@@ -1,9 +1,21 @@
 import os
+from pypdf import PdfReader
 import pypdfium2 as pdfium
 
 import config
 
-def get_text(fname:str) -> str:
+def get_text_pypdf(fname:str) -> str:
+    '''
+    pdfファイルのパスを渡すとpypdfで読んだtextを返す'''
+    reader = PdfReader(fname)
+    text = str()
+
+    for page in reader.pages:
+        text += page.extract_text()
+    
+    return text
+
+def get_text_pdfium(fname:str) -> str:
     '''
     pdfファイルのパスを渡すとpdfiumで読んだtextを返す'''
     reader = pdfium.PdfDocument(fname)
@@ -23,7 +35,8 @@ def pdf2text(pdfdir:str, textdir:str) -> None:
         # pdfの読み込み
         pdfpath = os.path.join(pdfdir, file)
         print(pdfpath)
-        text = get_text(pdfpath)
+        # text = get_text_pdfium(pdfpath)
+        text = get_text_pypdf(pdfpath)
 
         # textでの保存
         textpath = os.path.join(textdir, file+'.txt')
@@ -32,4 +45,4 @@ def pdf2text(pdfdir:str, textdir:str) -> None:
 
 if __name__ == '__main__':
     pdf2text(config.train_pdf_dir, config.train_text_dir)
-    pdf2text(config.valid_pdf_dir, config.valid_text_dir)
+    # pdf2text(config.valid_pdf_dir, config.valid_text_dir)
