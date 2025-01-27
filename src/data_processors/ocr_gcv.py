6e834f1569396802cfa792e2c7ba2ae30aec5b95
androiderNN <kento.i.1103@gmail.com>
2025-01-28 06:18:17 +0900

    google cloudのapiによるocrを実装 pypdf等によるテキスト化よりも精度は高そう テキストの座標情報も取得できるので、事後処理の幅も増える

---
+++ src/data_processors/ocr_gcv.py
@@ -0,0 +1,76 @@
+import os
+import time
+import pickle
+from google.cloud import vision
+
+def get_response(path:str):
+    '''
+    google cloud vision apiでocrリクエストを送り、そのレスポンスを返す'''
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+    return response
+
+def save_response(imgdir:str, ocrdir:str) -> None:
+    '''
+    画像と保存先のパスを渡すと、各画像のocr結果の生データをpickleで保存する'''
+    pdflist = [i for i in os.listdir(imgdir) if os.path.isdir(os.path.join(imgdir, i))]
+
+    for pdf in pdflist: # pdfファイルのループ
+        pdf_imgdir = os.path.join(imgdir, pdf)  # あるpdfの画像が格納されたディレクトリ
+        pdf_ocrdir = os.path.join(ocrdir, pdf)  # あるpdfのocr結果を格納するディレクトリ
+
+        print(pdf_imgdir)
+        
+        if not os.path.exists(pdf_ocrdir):
+            os.mkdir(pdf_ocrdir)
+        
+        imglist = [i for i in os.listdir(pdf_imgdir) if i[-4:]=='.jpg']
+        
+        for img in imglist: # 画像ファイルのループ
+            imgpath = os.path.join(pdf_imgdir, img)
+            pklpath = os.path.join(pdf_ocrdir, img+'.pkl')
+
+            if os.path.exists(pklpath): # apiの節約
+                continue
+
+            # ocrと結果の保存
+            response = get_response(imgpath)
+            pickle.dump(response, open(pklpath, 'wb'))
+
+            time.sleep(0.5)
+
+def detect_text(path):
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print("Texts:")
+
+    for text in texts:
+        print(f'\n"{text.description}"')
+
+        vertices = [
+            f"({vertex.x},{vertex.y})" for vertex in text.bounding_poly.vertices
+        ]
+
+        print("bounds: {}".format(",".join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+
+    return texts
--- src/process_data.py
+++ src/process_data.py
@@ -1,14 +1,18 @@
 import os
 import config
-from data_processors import company_extractor, pdf2text, ocr
+from data_processors import company_extractor, pdf2text, pdf2jpg, ocr_gcv
 
 if __name__ == '__main__':
     # 企業名のリスト作成
     # company_extractor.save_company_list(config.train_company_list, config.valid_company_list)
 
     # pdfをjpgに変換
-    # ocr.save_jpg(config.train_pdf_dir, config.train_img_dir)
-    # ocr.save_jpg(config.valid_pdf_dir, config.valid_img_dir)
+    # pdf2jpg.save_jpg(config.train_pdf_dir, config.train_img_dir)
+    # pdf2jpg.save_jpg(config.valid_pdf_dir, config.valid_img_dir)
+
+    # vision apiでocr
+    # ocr_gcv.save_response(config.train_img_dir, config.train_ocr_dir)
+    # ocr_gcv.save_response(config.valid_img_dir, config.valid_ocr_dir)
 
     # pdfをテキストに変換
     pdf2text.pdf2text(config.train_pdf_dir, config.train_text_dir)
