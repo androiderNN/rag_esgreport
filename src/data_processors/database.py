b823c936687fba28f1aa1efe48dbd58b115147be
androiderNN <kento.i.1103@gmail.com>
2025-01-31 15:09:02 +0900

    azureのapiによるembedding

--- src/data_processors/database.py
+++ src/data_processors/database.py
@@ -38,17 +38,19 @@ def save_db(name:str, texts:list[str], path:str) -> None:
 
     collection.add(
         documents=texts,
-        # embeddings=azure_embedding_fn(texts),
+        embeddings=azure_embedding_fn(texts),
         ids=[str(i) for i in range(len(texts))]
     )
 
-def text2db(textdir:str, dbdir:str, chunk_size:int =200, chunk_overlap:int =10) -> None:
+def text2db(textdir:str, dbdir:str, chunk_size:int =400, chunk_overlap:int =30) -> None:
     '''
     全ファイルのデータベースを保存する'''
+    print(textdir)
     texts = [f for f in os.listdir(textdir) if f[-4:]=='.txt']
 
     for txtfile in texts:
         print(txtfile)
+
         dbname = os.path.splitext(txtfile)[0]
         textpath = os.path.join(textdir, txtfile)
         dbpath = os.path.join(dbdir, dbname)
@@ -73,13 +75,14 @@ def load_db(path:str) -> chromadb.Collection:
     collection = client.get_collection(name)
     return collection
 
-def get_context(db_path:str, query:str, n_results:str =5):
+def get_context(db_path:str, query:str, n_results:str =3) -> list[str]:
     '''
     databaseのパスとクエリ、返答数を渡すと、関連するドキュメントを検索して返す'''
     db = load_db(db_path)
 
     result = db.query(
         query_texts=query,
+        query_embeddings=azure_embedding_fn(query),
         n_results=n_results
     )
-    return result
+    return result['documents'][0]
