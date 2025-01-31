7ae05d066aa2951a53d65a2278e2558c7eb7a03c
androiderNN <kento.i.1103@gmail.com>
2025-01-31 15:12:22 +0900

    プロンプト作成とazure openai apiへの問い合わせ実装 プロンプト自体はおそらく大きな問題はない データベースの精度改善が課題

---
+++ src/llm/getresponse_azure.py
@@ -0,0 +1,28 @@
+import os
+from openai import AzureOpenAI
+
+AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
+API_VERSION = os.getenv("API_VERSION")
+DEPLOYMENT_ID_FOR_CHAT_COMPLETION = os.getenv("DEPLOYMENT_ID_FOR_CHAT_COMPLETION")
+
+def getresponse(prompt):
+    client = AzureOpenAI(
+        api_key=AZURE_OPENAI_API_KEY,
+        azure_endpoint=AZURE_OPENAI_ENDPOINT,
+        api_version=API_VERSION
+    )
+
+    response = client.chat.completions.create(
+        model=DEPLOYMENT_ID_FOR_CHAT_COMPLETION,
+        messages=prompt,
+        max_tokens=4000,     # 応答に使えるトークン数上限
+        temperature=0.7,     # 創造性（0~1）
+        top_p=0.95,          # nucleus sampling
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None,
+        stream=False
+    ).choices[0].message.content  # 応答本文
+
+    return response
