c5fcbca9eced98f80b28ad03e00a2b7a0c6ad647
androiderNN <kento.i.1103@gmail.com>
2025-02-05 14:26:38 +0900

    OCRと画像両方からマークダウンを作成するよう修正 ヘッダーについてはプロンプトで明示的に指示するとうまく除外してくれた スコアはほぼ伸びず

--- src/data_processors/markdownize.py
+++ src/data_processors/markdownize.py
@@ -4,6 +4,7 @@ import vertexai
 from vertexai.preview.generative_models import GenerativeModel, Image
 
 VERTEX_GENMODEL = os.getenv('VERTEX_GENMODEL')
+genmodel = GenerativeModel(VERTEX_GENMODEL)
 
 def get_markdown(imagepath:str, ocrpath:str):
     '''
@@ -15,24 +16,17 @@ def get_markdown(imagepath:str, ocrpath:str):
         text = f.read()
 
     # プロンプト
-    # prompt = [
-    #     '下記の文章を構造化してmarkdown形式で出力してください。\n'
-    #     '文章は、添付の画像からOCRで抽出したものです。文章と画像両方を参照して適切な構造化を行ってください。\n'
-    #     '回答には本文のみを含めるようにしてください。\n' + \
-    #     text,
-    #     image
-    # ]
-
     prompt = [
-        '下記の画像から文章を読み取り、その結果を構造化してmarkdown形式で出力してください。\n'
-        '回答には本文のみを含めるようにしてください。\n',
-        image
+    '下記の文章を構造化してmarkdown形式で出力してください。\n'
+    '文章は、添付の画像からOCRで抽出したものです。文章と画像両方を参照して適切な構造化を行ってください。\n'
+    'この際、書類のヘッダーと思われる部分は無視し、必要な範囲の文章のみを使用してください。'
+    '回答には本文のみを含めるようにしてください。\n\n' + \
+    text,
+    image
     ]
 
     # マークダウン化
-    model = GenerativeModel(VERTEX_GENMODEL)
-    response = model.generate_content(prompt)
-
+    response = genmodel.generate_content(prompt)
     return response.candidates[0].content.parts[0].text
 
 def markdownize(imgdir, ocrdir:str, mddir:str) -> None:
@@ -70,4 +64,4 @@ def markdownize(imgdir, ocrdir:str, mddir:str) -> None:
             with open(mdpath, mode='w') as f:
                 f.write(md)
 
-            time.sleep(10)
+            time.sleep(1)
